9b34896e6e347f1b7bc3a7bcdf4185a02c412f77
Thai Nguyen <discoverylight@yahoo.com>
2017-11-13 18:15:48 -0800

    initial release

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <title>Annie</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "stylesheet" href = "{{ asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" type = "text/css" href = "{{ asset('css/font-awesome.css')}}">
+    <link rel = "stylesheet" href = "{{ asset('css/main.css')}}">
+    <script src="https://use.typekit.net/nax5cff.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+</head>
+<body>
+    <div class = "container">
+        <div class ="row mt-4 mb-4">
+            <div class = "col-md-4">
+                <!--Visible on medium display and up only-->
+                <div class="card d-none d-md-block">
+                    <img class="card-img-top" src = "{{ asset('images/annie_portrait.png') }}" alt="annie">
+                    <div class="card-body">
+                        <h2 class = "card-title text-center display-3 tk-mr-eaves-xl-modern">Annie Le</h2>
+                        <p class = "card-text contact"><i class = "fa fa-envelope-o fa-lg"></i>&nbsp;anniele1907[at]gmail[dot]com</p>
+                    </div>
+                </div>
+            </div>
+            <div class = "col-md-8 col-xs-12">
+                <!--Visible on portrait phone only and hidden on all-->
+                <div class="card mb-3 d-block d-sm-none">
+                    <img class="card-img-top" src = "{{ asset('images/annie_portrait.png') }}" alt="annie">
+                    <div class="card-body">
+                        <h2 class = "card-title text-center display-3 tk-mr-eaves-xl-modern">Annie Le</h2>
+                        <p class = "card-text contact"><i class = "fa fa-envelope-o fa-lg d-none d-sm-block"></i>&nbsp;anniele1907[at]gmail[dot]com</p>
+                    </div>
+                </div>
+                <div class = "card sky-background">
+                    <div class = "card-body">
+                
+                        <p class = "card-text">Annie is an aspiring nursing student who will complete her nursing associate degree in December 2017. She is an honor student who has devoted a lot of time toward her education while maintaining a full time job.  She wishes to apply her earned skills and knowledge to assist her employers to handle many medical situations.</p>
+                        <p class = "card-text">
+                    <h4 class = "tk-nimbus-sans"><i class = "fa fa-certificate fa-lg"></i>&nbsp;Licensure and Certitications</h4>
+                    <hr>
+                    <div class = "row justify-content-between">
+                        <div class = "col-8">
+                            <h6>Registered Nurse, California Board of Nursing</h6>
+                        </div>
+                        <div class = "col-4">
+                            <div class = "float-right">
+                                <h6 class = "time-text">Expected Feburary 2018</h6>
+                            </div>
+                            
+                        </div>
+
+
+                    </div>
+                    <div class = "row justify-content-between mt-3">
+                        <div class = "col-md-6">
+                            <h6>C.P.R. Certification</h6>
+                        </div>
+                        <div class = "col-md-6">
+                            <div class = "float-right">
+                                <h6 class = "time-text">When</h6>
+                            </div>
+                            
+                        </div>
+    
+
+                    </div>
+                    <div class = "row justify-content-between mt-3">
+                        <div class = "col-md-10">
+                            <h6>L.A. Hospital Fire and Life Safety Certification, UCLA-Olive View Hospital</h6>
+                        </div>
+                        <div class = "col-md-2">
+                            <div class = "float-right">
+                                <h6 class = "time-text">When</h6>
+                            </div>
+                            
+                        </div>
+    
+
+                    </div>
+                    <div class = "row justify-content-between mt-3">
+                        <div class = "col-10">
+                            <h6>Management of Assaultive Behavior Certification</h6>
+                        </div>
+                        <div class = "col-2">
+                            <div class = "float-right">
+                                <h6 class = "time-text">When</h6>
+                            </div>
+                            
+                        </div>
+    
+
+                    </div>
+                </p>
+                <p class = "card-text">
+                    <h4 class = "tk-nimbus-sans"><i class = "fa fa-graduation-cap fa-lg"></i>&nbsp; Education</h4>
+                    <hr>
+                    <div class="row">
+                        <div class = "col-md-8">
+                            <h5>Los Angeles Valley College, California</h5>
+                            <p>Associate Degree in Nursing</p>
+                            <ul>
+                                <li>Current GPA: <span class = "text-danger">3.7</span></li>
+                                <li>Dean's Honor List: Fall 2016
+                            </ul>
+                            <h5>Los Angeles Pierce College, California</h5>
+                            <ul>
+                                <li>President's Honor List: 3 semesters</li>
+                                <li>Dean's Honor List: 5 semesters</li>
+                            </ul>
+                        </div>
+                        <div class = "col-md-4">
+                            <div class = "float-right">
+                                <h6 class = "time-text">Graduation: Dec. 2017</h6>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                </p>
+                <p class = "card-text">
+                    <h4 class = "tk-nimbus-sans"><i class = "fa fa-trophy fa-lg"></i>&nbsp; Scholarships</h4>
+                    <hr>
+                    <div class = "row">
+                        <div class = "col-md-10">
+                            <h6>Michael D. Antonovich RN Student Scholarship, County of Los Angeles Department of Health</h6>
+                        </div>
+                        <div class = "col-md-2">
+                            <div class = "float-right">
+                                <h6 class = "time-text">Oct. 2017</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class = "row mt-3">
+                        <div class = "col-md-10">
+                            <h6>Deloras Jones RN Scholarship Program, Scholarship America</h6>
+                        </div>
+                        <div class = "col-md-2">
+                            <div class = "float-right">
+                                <h6 class = "time-text">Nov. 2016</h6>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                    
+                <p>
+                <p class = "card-text">
+                    <h4 class = "tk-nimbus-sans"><i class = "fa fa-heart fa-lg"></i>&nbsp;Clinical Experience</h4>
+                    <hr>
+                    <div class = "row justify-content-between">
+                        <div class = "col-md-8">
+                            <h6>Valley Presbyterian Hospital, Los Angeles</h6>
+                            <p>Position: Student Nurse</p>
+                            <!-- <ul>
+                                <li>Job</li>
+                                <li>Job</li>
+                            </ul> -->
+                        </div>
+                        <div class = "col-md-4">
+                            <div class = "float-right">
+                                <h6 class = "time-text">Fall 2017, Fall 2016, Spring 2016</span></h6>
+                            </div>
+                            
+                        </div>
+
+                    </div>
+                    <div class = "row justify-content-between">
+                        <div class = "col-md-8">
+                            <h6>Northridge Hospital Medical Center, Los Angeles</h6>
+                            <p>Position: Student Nurse</p>
+                            <!-- <ul>
+                                <li>Job</li>
+                                <li>Job</li>
+                            </ul> -->
+                        </div>
+                        <div class = "col-md-4">
+                            <div class = "float-right">
+                                <h6 class = "time-text">Fall 2017</h6>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class = "row justify-content-between">
+                        <div class = "col-md-10">
+                            <h6>Kaiser Permanente West Los Angeles Medical Center, Los Angeles</h6>
+                            <p>Position: Student Nurse</p>
+                            <!-- <ul>
+                                <li>Job</li>
+                                <li>Job</li>
+                            </ul> -->
+                        </div>
+                        <div class = "col-md-2">
+                            <div class = "float-right">
+                                <h6 class = "time-text">Fall 2016</h6>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class = "row justify-content-between">
+                        <div class = "col-md-10">
+                            <h6>Kaiser Permanente Panorama City Medical Center, Los Angeles</h6>
+                            <p>Position: Student Nurse</p>
+                            <!-- <ul>
+                                <li>Job</li>
+                                <li>Job</li>
+                            </ul> -->
+                        </div>
+                        <div class = "col-md-2">
+                            <div class = "float-right">
+                                <h6 class = "time-text">Fall 2016</h6>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class = "row justify-content-between">
+                        <div class = "col-md-10">
+                            <h6>Motion Picture & Television Country House and Hospital, Los Angeles</h6>
+                            <p>Position: Student Nurse</p>
+                            <!-- <ul>
+                                <li>Job</li>
+                                <li>Job</li>
+                            </ul> -->
+                        </div>
+                        <div class = "col-md-2">
+                            <div class = "float-right">
+                                <h6 class = "time-text">Fall 2016</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class = "row justify-content-between">
+                        <div class = "col-md-10">
+                            <h6>Gateways Hospital and Mental Health Center, Los Angeles</h6>
+                            <p>Position: Student Nurse</p>
+                            <!-- <ul>
+                                <li>Job</li>
+                                <li>Job</li>
+                            </ul> -->
+                        </div>
+                        <div class = "col-md-2">
+                            <div class = "float-right">
+                                <h6 class = "time-text">Fall 2016</h6>
+                            </div>
+                        </div>
+                    </div>
+        
+                </p>
+                <p class = "card-text">
+                    <h4 class = "tk-nimbus-sans"><i class = "fa fa-briefcase fa-lg"></i>&nbsp;Work Experience</h4>
+                    <hr>
+                    <div class = "row justify-content-between">
+                        <div class = "col-md-8">
+                            <h6>Sugar Nails, Los Angeles</h6>
+                            <p>Manicurist Technician</p>
+                            <ul>
+                                <li>Delivers top-notch nail care services to clients</li>
+                                <li>Supports safety and health procedures for clients</li>
+                            </ul>
+                        </div>
+                        <div class = "col-md-4">
+                            <div class = "float-right">
+                                <h6 class = "time-text">Nov. 2013 to Present</span></h6>
+                            </div>
+                            
+                        </div>
+
+                    </div>
+                </p>
+            </div>
+        </div>
+            </div>
+        </div>
+        
+        
+
+    </div>
+    
+    <footer class = "container-fluid">
+        <div class = "row justify-content-center carbon-background">
+            <div class = "col-md-4 col-xs-4 text-center">
+                <p class = "tk-mr-eaves-xl-modern mt-4">
+                    Designed by <a href = "http://thaiqnguyen.com" target="_blank">Thai Nguyen</a>
+                </p>
+            </div>
+            
+        </div>
+        
+    </footer>
+    
+    
+
+<script src = "{{ asset('js/main.js')}}"></script>
+
+</body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,20 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+ mix.browserSync({
+    open:'external',
+    proxy:'local.annie.dev:8888'
+});
+
+mix.js('resources/assets/js/main.js', 'public/js');
+   
