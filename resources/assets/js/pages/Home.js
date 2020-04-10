e76b89111436a3cf44756d62e1b3518387c4caa0
Thai Nguyen <discoverylight@yahoo.com>
2020-04-09 20:46:23 -0700

    setup Home page

--- resources/assets/js/pages/Home.js
+++ resources/assets/js/pages/Home.js
@@ -1,7 +1,40 @@
 import React from "react";
+import {
+	Box,
+	Container,
+	CssBaseline,
+	Grid,
+	Paper,
+	Typography
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({}));
 const Home = () => {
-	return <div></div>;
+	const classes = useStyles();
+
+	return (
+		<div>
+			<CssBaseline />
+			<Container maxWidth="lg">
+				<Grid container spacing={2}>
+					<Grid item xs={12} sm={6}>
+						<Box>
+							<Typography>
+								<strong>Annie Le</strong> is a registered nurse
+								at Los Angeles County + USC Medical Center
+							</Typography>
+						</Box>
+					</Grid>
+					<Grid item xs={12} sm={6}>
+						<Box>
+							<Typography>Photo goes here</Typography>
+						</Box>
+					</Grid>
+				</Grid>
+			</Container>
+		</div>
+	);
 };
 
 export default Home;
